dbefef105d9c338d167c5b3c68607cc5aedfcdc4
Keshav Pawar <keshavpawar784@gmail.com>
2026-04-27 10:39:31 +0530

    added ssl

--- Palindrome Checker/Backend/db.js
+++ Palindrome Checker/Backend/db.js
@@ -8,7 +8,10 @@ dotenv.config();
     host:process.env.DB_HOST,
     database:process.env.DB_NAME, 
     password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT
+    port:process.env.DB_PORT,
+    ssl:{
+        rejectUnauthorized: false
+    }
 })
 // console.log("Password:",process.env.DB_PASSWORD)
 // console.log("Type:", typeof process.env.DB_PASSWORD)
